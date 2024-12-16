60317a0e7fd3114f6d7cf831c56f0e3cfb2d35e5
nutchyleo <nutchapon.m@thaibev.com>
2024-12-16 16:43:58 +0700

    fix: theme changer

--- src/plugins/themeChanger.ts
+++ src/plugins/themeChanger.ts
@@ -3,14 +3,37 @@ const themeChanger = async function themeController(
   pathToThemeFolder: string
 ) {
   const name = themeName.toLowerCase();
-  const linkElement = document.createElement("link");
   const links = document.querySelectorAll(`link[href*="${pathToThemeFolder}"]`);
-  if (links.length > 0) {
-    links.forEach((link) => link.remove());
+  if (name === "default") {
+    //remove all link href start with pathToThemeFolder
+    if (links.length > 0) {
+      links.forEach((link) => link.remove());
+    }
+  } else {
+    const linkElement = document.createElement("link");
+    const links = document.querySelectorAll(
+      `link[href*="${pathToThemeFolder}"]`
+    );
+    if (links.length > 0) {
+      links.forEach((link) => link.remove());
+    }
+    linkElement.rel = "stylesheet";
+    linkElement.href = pathToThemeFolder + name + ".css";
+    document.head.appendChild(linkElement);
   }
-  linkElement.rel = "stylesheet";
-  linkElement.href = pathToThemeFolder + name + ".css";
-  document.head.appendChild(linkElement);
 };
 
 export default themeChanger;
+
+export function getCurrentThemeName(pathToThemeFolder: string) {
+  const link = document.querySelector(`link[href*="${pathToThemeFolder}"]`);
+  let theme = "default";
+  if (link) {
+    const linkHref = link.getAttribute("href");
+    if (linkHref) {
+      theme =
+        linkHref.split("/").pop()?.split(".")[0]?.toLowerCase() || "default";
+    }
+  }
+  return theme;
+}
